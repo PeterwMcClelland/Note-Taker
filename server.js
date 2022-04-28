aadc78b944f3a6d91e7fa962cab001ef11d4424b
Peter McClelland <peterwmcclelland@gmail.com>
2022-04-27 19:10:20 -0500

    test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
+const PORT = process.env.PORT || 3001;
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 3002;
 
 const fs = require('fs');
 const path = require('path');
